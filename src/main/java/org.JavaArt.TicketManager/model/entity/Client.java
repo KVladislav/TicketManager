a56874e7a16125699cc1bf5c5357c8ffa0c411b7
Vladislav Karpenko <vladislav@karpenko.od.ua>
2014-06-12 13:29:30 +0300

    fix Client field - name/surename - name delete Client field telephone

--- src/main/java/org.JavaArt.TicketManager/model/entity/Client.java
+++ src/main/java/org.JavaArt.TicketManager/model/entity/Client.java
@@ -39,28 +39,12 @@ public class Client {
         this.id = id;
     }
 
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public String getSureName() {
-        return sureName;
-    }
-
-    public void setSureName(String sureName) {
-        this.sureName = sureName;
     }
 
     public String getDescription() {
