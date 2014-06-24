7c4f7faea7224d24a1fc143067ed08413e262951
Vladislav Karpenko <vladislav@karpenko.od.ua>
2014-06-24 20:25:05 +0300

    fix menu

---
+++ src/main/java/org/JavaArt/TicketManager/model/entity/org/JavaArt/TicketManager/controllers/WelcomeController.java
@@ -0,0 +1,10 @@
+package org.JavaArt.TicketManager.model.entity.org.JavaArt.TicketManager.controllers;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Vladislav Karpenko
+ * Date: 18.06.2014
+ * Time: 13:27
+ */
+public class WelcomeController {
+}
