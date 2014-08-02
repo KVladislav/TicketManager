fcdffd5de2062dbfd1eea850d75058bd45255e3f
Vladislav Karpenko <vladislav@karpenko.od.ua>
2014-08-02 19:49:01 +0300

    fix posgress conn credintals

--- src/main/java/org/JavaArt/TicketManager/controllers/AboutController.java
+++ src/main/java/org/JavaArt/TicketManager/controllers/AboutController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class AboutController {
 
+
+
     @RequestMapping("/about")
     public String showIndex(ModelMap model) {
         model.addAttribute("pageName", 0);
